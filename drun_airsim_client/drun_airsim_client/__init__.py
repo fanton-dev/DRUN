7ea30a53bcf9586bb1da044fdb3bd592c0b3563b
Angel Penchev <angel.penchev.tsb@gmail.com>
2020-10-02 14:39:59 +0300

    [Feat] DRUN AirSim Client - set_time
    Created a function for setting the time in the simulation.
    References #30.

--- drun_airsim_client/drun_airsim_client/__init__.py
+++ drun_airsim_client/drun_airsim_client/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import absolute_import
 
 from airsim import (
-    MultirotorClient
+    MultirotorClient,
 )
 
 
@@ -16,8 +16,29 @@ class DRUNAirSimClient(MultirotorClient):
         port (int, optional): Port of the AirSim instance. Defaults to 41451.
     """
 
-    def __init__(self, ip: str = "", port: int = 41451):
+    def __init__(self, ip: str = "", port: int = 41451) -> None:
         MultirotorClient.__init__(self)
         MultirotorClient.confirmConnection(self)
         self.enableApiControl(True)
         self.armDisarm(True)
+
+    def set_time(
+            self,
+            start_datetime: str,
+            clock_speed: float = 1.0
+    ) -> None:
+        """Sets AirSim time properties.
+
+        Args:
+            start_datetime (str): Time in "YYYY-MM-DD hh:mm:ss" format.
+            clock_speed (float, optional): Time scale factor. Defaults to 1.0.
+        """
+
+        self.simSetTimeOfDay(
+            True,
+            start_datetime=start_datetime,
+            is_start_datetime_dst=True,
+            celestial_clock_speed=clock_speed,
+            update_interval_secs=60,
+            move_sun=True
+        )
