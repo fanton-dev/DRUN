2833a6c375eff3a75a16e1d511b4de88a6df3d32
Simeon Georgiev <simeongeorgiev09@gmail.com>
2020-10-02 17:54:54 +0300

    [Refactor] ImageThread - network_output
    Renamed sock to ssock.
    References #4.

--- communication/src/drone/image/output.py
+++ communication/src/drone/image/output.py
@@ -28,8 +28,8 @@ def network_output(
         port (int): TCP port on the server for communication.
         fps (float, optional): Rate data to be send at. Defaults to 10.0.
     """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((ip_address, port))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ssock:
+        ssock.connect((ip_address, port))
 
         for _ in every(1/fps):
             # current_image can be read on the server
