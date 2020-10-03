5f3d1234075893d7656356f4bd32df0d9ec3ee2a
Angel Penchev <angel.penchev.tsb@gmail.com>
2020-10-04 01:51:14 +0300

    [Feat] ReplayMemory
    Created a replay memory class with a deque for storing experiences.
    Resolved #53.

--- ai/drone_regular.py
+++ ai/drone_regular.py
@@ -29,11 +29,6 @@ BATCH_SIZE = 64
 PRETRAIN_LENGTH = BATCH_SIZE
 MEMORY_SIZE = 1000000
 
-# Epsilon greedy strategy
-EXPLORE_START = 1.0
-EXPLORE_STOP = 0.01
-DECAY_RATE = 0.0001
-
 # Q-learning hyperparameters
 GAMMA = 0.95
 
@@ -106,3 +101,31 @@ def drun_dqn() -> Model:
 
 drone_dqn = drun_dqn()
 optimizer = Adam(learning_rate=LEARNING_RATE, clipnorm=1.0)
+
+Experience = namedtuple(
+    "Experience",
+    ("state", "action", "next_state", "reward")
+)
+
+
+class Memory():
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+        self.push_count = 0
+
+    def add(self, experience):
+        self.buffer.append(experience)
+        self.push_count += 1
+
+    def sample(self, batch_size):
+        buffer_size = len(self.buffer)
+        index = np.random.choice(
+            np.arange(buffer_size),
+            size=batch_size,
+            replace=False
+        )
+
+        return [self.buffer[i] for i in index]
+
+    def is_sample_available(self, batch_size):
+        return len(self.buffer) >= batch_size
