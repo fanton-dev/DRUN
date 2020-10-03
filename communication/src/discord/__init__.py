c5298183090bd1c1e8b3238b0c0c0563ee5097d2
bobig6 <b.i.ivanov1@gmail.com>
2020-10-03 23:35:30 +0300

    [feat] DM handler
    Resolved #49

--- communication/src/discord/__init__.py
+++ communication/src/discord/__init__.py
@@ -11,48 +11,52 @@ import asyncio
 from threading import Thread
 
 
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+from geopy.geocoders import Nominatim
 
 
-
-# bot = discord.Client()
-bot = commands.Bot(command_prefix = '!')
-
+bot = commands.Bot(command_prefix='!')
 
 
 def init() -> None:
-    #super(Discord, self).__init__()
+    load_dotenv(dotenv_path='.env')
+    token = os.getenv("DISCORD_BOT_TOKEN")
     loop = asyncio.get_event_loop()
-    loop.create_task(bot.start("NzYxOTAyNjUxNzI1NzA5MzUy.X3hXIw.74wHASY4lRQettEU549L6xcTZtE"))
+    loop.create_task(bot.start(token))
     t = Thread(target=loop.run_forever())
     t.start()
 
-# @bot.event
-# async def on_message(message):
-#     if message.author == bot.user:
-#         return
-#     channel = message.channel
-#     await channel.send("hi")
 
 @bot.command(pass_context=True)
 async def delivery(ctx):
     await ctx.send(f'Hello {ctx.author}, please tell us the phone number of the person, you want to deliver to')
     channel = ctx.channel
     msg = await bot.wait_for('message')
+    if msg.author == bot.user:
+        msg = await bot.wait_for('message')
     phone_number = msg.content
     await ctx.send(f'Thank you! Now please tell us where do you want us to pickup your package from?')
     msg = await bot.wait_for('message')
-    address = msg.content
+    if msg.author == bot.user:
+        msg = await bot.wait_for('message')
+    location = None
+    while location == None:
+        await ctx.send(f'We couldn\'t find that address please try again')
+        msg = await bot.wait_for('message')
+        if msg.author == bot.user:
+            msg = await bot.wait_for('message')
+        address = msg.content
+        geolocator = Nominatim(user_agent="drun")
+        location = geolocator.geocode(address)
     await ctx.send(f'And finally, how much do you want to charge the reciever?')
     msg = await bot.wait_for('message')
-    charge = msg.content
+    if msg.author == bot.user:
+        msg = await bot.wait_for('message')
+    price = msg.content
     await ctx.send(f'Affirmative! Your request has been made!')
     print(phone_number)
-    print(address)
-    print(charge)
-
-
-
-    
-    # user = bot.get_user(416531445398634496)
-    # print(bot)
-    # await user.send("Hi")
+    print(location.address)
+    print((location.latitude, location.longitude))
+    print(price)
