1b82c52a5a41f658a9c1dd30a178393b00ebd149
Angel Penchev <angel.penchev.tsb@gmail.com>
2021-01-13 03:52:51 +0200

    [Feat] POST "/api/orders" controller
    Added a controller function for handling requests to the POST endpoint.
    References #59.

---
+++ server/services/order-service/src/interfaces/controllers/post-order.ts
@@ -0,0 +1,43 @@
+import {HttpRequest} from '../../../../core/global';
+
+/**
+ * Express controller for handling of POST "/order".
+ *
+ * @export
+ * @param {{createOrder: Function}} {
+ *   createOrder
+ * } - dependency injection
+ * @return {Function} - post order controller builder function
+ */
+export default function makePostOrder({
+  createOrder,
+}: {createOrder: Function}) {
+  return async function postOrder(httpRequest: HttpRequest) {
+    try {
+      const {source = {}, ...orderInfo} = httpRequest.body;
+      source.ip = httpRequest.ip;
+      source.browser = httpRequest.headers['User-Agent'];
+      if (httpRequest.headers['Referer']) {
+        source.referrer = httpRequest.headers['Referer'];
+      }
+      const order = await createOrder(orderInfo);
+      return {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        statusCode: 201,
+        body: {order},
+      };
+    } catch (e) {
+      return {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        statusCode: 400,
+        body: {
+          error: e.message,
+        },
+      };
+    }
+  };
+}
