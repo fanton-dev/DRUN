75b2ed98e4defac02c89ffdc9485e5a5f6299f66
Angel Penchev <angel.penchev.tsb@gmail.com>
2021-02-03 06:44:00 +0200

    [Typo] Retrive order usecase
    Fixed a small typo i forgot about. :)
    References #59.

--- server/services/order-service/src/usecases/retrieve-order.ts
+++ server/services/order-service/src/usecases/retrieve-order.ts
@@ -1,17 +1,17 @@
-import {DatabaseController} from '../../../core/@types/global';
+import {OrderDatabaseController} from '../../../core/@types/global';
 
 /**
  * Retrieves information for a concrete order from the database.
  *
  * @export
- * @param {{ordersDatabase: DatabaseController}} {
+ * @param {{ordersDatabase: OrderDatabaseController}} {
  *   ordersDatabase,
  * } - dependency injection
  * @return {Function} - order retrieval function
  */
 export default function buildRetrieveOrder({
   ordersDatabase,
-}: {ordersDatabase: DatabaseController}): Function {
+}: {ordersDatabase: OrderDatabaseController}): Function {
   return async function retrieveOrder(orderId: string) {
     return await ordersDatabase.findById(orderId);
   };
