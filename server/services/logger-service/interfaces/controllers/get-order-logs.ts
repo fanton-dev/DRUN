df3639df75b34fc4f9c3a163bd41cf62db6af76c
Angel Penchev <angel.penchev.tsb@gmail.com>
2021-02-07 17:26:28 +0200

    [Feat] getOrderLogs controller
    Added express controller for handling order logs retrival requests.
    References #65.

---
+++ server/services/logger-service/interfaces/controllers/get-order-logs.ts
@@ -0,0 +1,36 @@
+import {HttpRequest} from '../../../core/@types/global';
+
+/**
+ * Express controller for handling of GET "/logs/order".
+ *
+ * @export
+ * @param {Function} retrieveOrderLogs - order logs retrieval dependency
+ * @return {Function} - get order logs controller builder function
+ */
+export default function makeGetOrderLogs({
+  retrieveOrderLogs,
+}: {retrieveOrderLogs: Function}): Function {
+  return async function getOrderLogs(httpRequest: HttpRequest) {
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+    try {
+      const getOrder = await retrieveOrderLogs(
+          httpRequest.query.orderId,
+      );
+      return {
+        headers,
+        statusCode: 200,
+        body: getOrder,
+      };
+    } catch (e) {
+      return {
+        headers,
+        statusCode: 400,
+        body: {
+          error: e.message,
+        },
+      };
+    }
+  };
+}
