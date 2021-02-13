ad1332ebadf15a9e1133799963858cc3b7a22923
Angel Penchev <angel.penchev.tsb@gmail.com>
2021-02-13 15:34:20 +0200

    [Feat][Test] UserAuthenticationLocalSourceImpl
    Implemented local cache credentials source.
    References #69.

--- app/lib/features/authentication/data/sources/user_authentication_local_source.dart
+++ app/lib/features/authentication/data/sources/user_authentication_local_source.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+
+import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../../core/errors/exceptions.dart';
 import '../models/user_credentials_model.dart';
 
 abstract class UserAuthenticationLocalSource {
@@ -8,4 +14,34 @@ abstract class UserAuthenticationLocalSource {
   Future<UserCredentialsModel> getUserCredentials();
 
   Future<void> cacheUserCredentials(UserCredentialsModel userCredentials);
+}
+
+const CACHE_KEY = 'CACHED_USER_CREDENTIALS';
+
+class UserAuthenticationLocalSourceImpl
+    implements UserAuthenticationLocalSource {
+  final SharedPreferences sharedPreferences;
+
+  UserAuthenticationLocalSourceImpl({
+    @required this.sharedPreferences,
+  });
+
+  @override
+  Future<UserCredentialsModel> getUserCredentials() {
+    final jsonString = sharedPreferences.getString(CACHE_KEY);
+
+    if (jsonString == null) {
+      throw CacheException();
+    }
+
+    return Future.value(UserCredentialsModel.fromJSON(json.decode(jsonString)));
+  }
+
+  @override
+  Future<void> cacheUserCredentials(
+    UserCredentialsModel userCredentials,
+  ) async {
+    final jsonString = json.encode(userCredentials.toJSON());
+    return sharedPreferences.setString(CACHE_KEY, jsonString);
+  }
 }
---
+++ app/test/features/authentication/data/sources/user_authentication_local_source_test.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+
+import 'package:DRUN/core/errors/exceptions.dart';
+import 'package:DRUN/features/authentication/data/models/user_credentials_model.dart';
+import 'package:DRUN/features/authentication/data/sources/user_authentication_local_source.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../../fixtures/fixture_parser.dart';
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
+
+const CACHE_KEY = 'CACHED_USER_CREDENTIALS';
+
+void main() {
+  UserAuthenticationLocalSourceImpl dataSource;
+  MockSharedPreferences mockSharedPreferences;
+
+  setUp(() {
+    mockSharedPreferences = MockSharedPreferences();
+    dataSource = UserAuthenticationLocalSourceImpl(
+      sharedPreferences: mockSharedPreferences,
+    );
+  });
+
+  group('getUserCredentials', () {
+    final tUserCredentialsModelSuccess = UserCredentialsModel.fromJSON(
+      json.decode(fixture('user_credentials_success.json')),
+    );
+
+    test(
+      'should return UserCredentialsModel from SharedPreferences when there is one cached',
+      () async {
+        // Arrange
+        when(mockSharedPreferences.getString(any))
+            .thenReturn(fixture('user_credentials_success.json'));
+
+        // Act
+        final result = await dataSource.getUserCredentials();
+
+        // Assert
+        verify(mockSharedPreferences.getString(CACHE_KEY));
+        expect(result, tUserCredentialsModelSuccess);
+      },
+    );
+
+    test(
+      'should throw CacheException when there are no user credentials cached',
+      () async {
+        // Arrange
+        when(mockSharedPreferences.getString(any)).thenReturn(null);
+
+        // Act
+        final call = dataSource.getUserCredentials;
+
+        // Assert
+        expect(() => call(), throwsA(isA<CacheException>()));
+      },
+    );
+  });
+
+  group('cacheUserCredentials', () {
+    final tUserCredentialsModelSuccess = UserCredentialsModel(
+      userId: '4ade5874-c573-4c8f-b2b8-7db5fccd983b',
+      userToken:
+          '713ADC1F515B3E0BBDF964DBAE6257A5B8A617115816A1705EACF9C00394A5',
+    );
+
+    test(
+      'should call SharedPreferences to cache the data',
+      () async {
+        // Act
+        dataSource.cacheUserCredentials(tUserCredentialsModelSuccess);
+
+        // Assert
+        final jsonString = json.encode(tUserCredentialsModelSuccess.toJSON());
+        verify(mockSharedPreferences.setString(CACHE_KEY, jsonString));
+      },
+    );
+  });
+}
