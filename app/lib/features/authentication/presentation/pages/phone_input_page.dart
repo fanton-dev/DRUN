cca7ba94ab003c8a34bdf71f0927f5544da9b5e6
Angel Penchev <angel.penchev.tsb@gmail.com>
2021-02-20 22:57:34 +0200

    [Feat] PhoneInputPage
    Added a page where the user inputs their phone number.
    Added display for any failures returned.
    References #69.

--- app/lib/features/authentication/presentation/pages/phone_input_page.dart
+++ app/lib/features/authentication/presentation/pages/phone_input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/widgets.dart';
+
 class PhoneInputPage extends StatelessWidget {
   final String error;
   const PhoneInputPage({Key key, this.error}) : super(key: key);
@@ -7,7 +9,25 @@ class PhoneInputPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Placeholder(),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            HeadingMessage(
+              height: MediaQuery.of(context).size.height / 4,
+              heading: 'Enter your phone number',
+              subHeading: 'We will send you a SMS with an authentication code',
+            ),
+            SizedBox(height: 10.0),
+            FailureMessage(
+              height: MediaQuery.of(context).size.height / 16,
+              message: error,
+            ),
+            SizedBox(height: 20.0),
+            PhoneInputForm(),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ app/lib/features/authentication/presentation/widgets/phone_input_form.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_sim_country_code/flutter_sim_country_code.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+
+import 'widgets.dart';
+import '../bloc/authentication_bloc.dart';
+
+class PhoneInputForm extends StatefulWidget {
+  const PhoneInputForm({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _PhoneInputFormState();
+}
+
+class _PhoneInputFormState extends State<PhoneInputForm> {
+  bool isValidPhoneNumber;
+  String phoneNumber;
+
+  @override
+  void initState() {
+    isValidPhoneNumber = false;
+    super.initState();
+  }
+
+  void _setIsValidPhoneNumber(bool state) {
+    setState(() {
+      isValidPhoneNumber = state;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          FutureBuilder(
+            future: FlutterSimCountryCode.simCountryCode,
+            builder: (BuildContext context, AsyncSnapshot isoCodeSnapshot) {
+              return Container(
+                child: InternationalPhoneNumberInput(
+                  selectorConfig: SelectorConfig(
+                    selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
+                    useEmoji: true,
+                  ),
+                  onInputChanged: (PhoneNumber phoneNumber) {
+                    this.phoneNumber = phoneNumber.toString();
+                  },
+                  onInputValidated: (bool isValid) =>
+                      _setIsValidPhoneNumber(isValid),
+                  initialValue: PhoneNumber(
+                    isoCode: isoCodeSnapshot.data ?? 'BG',
+                  ),
+                ),
+              );
+            },
+          ),
+          SizedBox(height: 10.0),
+          PrimaryActionButton(
+            text: isValidPhoneNumber ? 'Continue' : 'Invalid number',
+            call: isValidPhoneNumber
+                ? () => BlocProvider.of<AuthenticationBloc>(context)
+                    .add(SendAuthenticationSmsEvent(this.phoneNumber))
+                : null,
+          ),
+        ],
+      ),
+    );
+  }
+}
