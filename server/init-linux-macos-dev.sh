bbb7df904077d5d7263543816e65e41ef5603458
Angel Penchev <angel.penchev.tsb@gmail.com>
2021-02-23 18:24:24 +0200

    [Feat] Shared core
    Created scripts for initalizing symlinks to the core in all services.
    References #72.

---
+++ server/init-linux-macos-dev.sh
@@ -0,0 +1,6 @@
+# Creates simlinks in all services to the core folder
+ln -s "./services/orders-service/core" "./core"
+ln -s "./services/payment-service/core" "./core"
+ln -s "./services/delivery-service/core" "./core"
+ln -s "./services/logger-service/core" "./core"
+ln -s "./services/users-service/core" "./core"
