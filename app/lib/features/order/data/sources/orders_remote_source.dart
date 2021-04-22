7632cb5168c5db13bd93761b9995ddb3bc7b4974
Angel Penchev <angel.penchev.tsb@gmail.com>
2021-04-22 08:00:47 +0300

    [Feat] OrdersRemoteSourceImpl.getPaymentCardToken
    Created a method for displaying a payment card form and generating the
    card token.
    References #71.

--- app/lib/features/order/data/sources/orders_remote_source.dart
+++ app/lib/features/order/data/sources/orders_remote_source.dart
@@ -1,4 +1,8 @@
+import 'package:DRUN/core/errors/exceptions.dart';
+import 'package:stripe_payment/stripe_payment.dart';
+
 import '../models/delivery_order_model.dart';
+import 'package:meta/meta.dart';
 
 abstract class OrdersRemoteSource {
   /// POST to https://$SERVER_ADDRESS/api/orders endpoint.
@@ -10,4 +14,28 @@ abstract class OrdersRemoteSource {
   ///
   /// Throws a [PaymentCardException] for all errors that occur.
   Future<String> getPaymentCardToken();
+}
+
+class OrdersRemoteSourceImpl implements OrdersRemoteSource {
+  final Future<PaymentMethod> Function() paymentRequestWithCardForm;
+
+  OrdersRemoteSourceImpl({
+    @required this.paymentRequestWithCardForm,
+  });
+
+  @override
+  Future<String> getPaymentCardToken() async {
+    try {
+      final paymentMethod = await paymentRequestWithCardForm();
+      return paymentMethod.id;
+    } catch (e) {
+      throw PaymentCardException();
+    }
+  }
+
+  @override
+  Future<String> makeDeliveryOrder(DeliveryOrderModel deliveryOrderModel) {
+    // TODO: implement makeDeliveryOrder
+    throw UnimplementedError();
+  }
 }
