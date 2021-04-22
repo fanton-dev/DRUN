b306de79bcfd5ca34efdb82ee69eed7e00ba335f
Angel Penchev <angel.penchev.tsb@gmail.com>
2021-04-22 07:24:57 +0300

    [Refactor] Moved several shared modules to 'core'

--- app/lib/features/authentication/presentation/widgets/widgets.dart
+++ app/lib/features/authentication/presentation/widgets/widgets.dart
@@ -1,5 +1,4 @@
 export 'code_input_form.dart';
-export 'failure_message.dart';
 export 'heading_message.dart';
 export 'phone_input_form.dart';
 export 'primary_action_button.dart';
--- app/lib/features/home/presentation/pages/pages.dart
+++ app/lib/features/home/presentation/pages/pages.dart
@@ -1,1 +1,2 @@
 export 'home_page.dart';
+export 'home_failure_page.dart';
